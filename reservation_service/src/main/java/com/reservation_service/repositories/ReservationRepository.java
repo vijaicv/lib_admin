f252c5b8a2aa0e032225ae2ed90296c882a04735
vijaicv <vijaicv@gmail.com>
2020-02-11 12:14:47 +0530

    reservation_service initial commit

---
+++ reservation_service/src/main/java/com/reservation_service/repositories/ReservationRepository.java
@@ -0,0 +1,14 @@
+package com.reservation_service.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.reservation_service.models.Reservation;
+
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Integer>{
+	List<Reservation> findByBookId(int bookId);
+}
---
+++ reservation_service/src/main/java/com/reservation_service/services/ReservationService.java
@@ -0,0 +1,48 @@
+package com.reservation_service.services;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import com.reservation_service.models.Reservation;
+import com.reservation_service.models.ReservationMessage;
+import com.reservation_service.repositories.ReservationRepository;
+
+
+
+@Component
+public class ReservationService {
+	
+	@Autowired
+	ReservationRepository reservationRepo;
+	
+	@Autowired
+	KafkaTemplate<String, ReservationMessage> template;
+	
+	public void reserve(int userId,int bookId) {
+		Reservation reservation = new Reservation();
+		reservation.setBookId(bookId);
+		reservation.setUserId(userId);
+		reservation.setDate(new Date());
+		
+		reservationRepo.save(reservation);
+	}
+	
+	
+	public void notifyReservedStudents(int bookId) {
+		List<Reservation> reservations = reservationRepo.findByBookId(bookId);
+		List<Integer> listOfReservedStudents = new ArrayList<Integer>();
+		
+		for(Reservation r : reservations) {
+			listOfReservedStudents.add(r.getUserId());
+		}
+		
+		ReservationMessage r_message = new ReservationMessage(listOfReservedStudents, "Book has returned");
+		template.send("newtopic", r_message);
+	}
+
+}
